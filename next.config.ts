29825fedd2bb24f414b122b665ba21e5d7ec2012
XuxuCommit <gleissnersuleika@gmail.com>
2026-07-30 18:44:20 +0000

    modified:   next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-
-  experimental: {
-    typedRoutes: true
-  },
-
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**"
-      }
-    ]
-  }
+  typedRoutes: true,
 };
 
 export default nextConfig;
